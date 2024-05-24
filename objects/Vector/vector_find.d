b9d2a02c2064d6898fa4dc1063bcb73739e4922e
Bryan Van paemel <bvan-pae@bess-f2r3s16.clusters.42paris.fr>
2024-05-24 17:29:11 +0200

    Added vector_push_sorted && vector_quick_sort

--- objects/Vector/vector_find.d
+++
@@ -1,7 +0,0 @@
-objects/Vector/vector_find.o: src/Vector/vector_find.c \
-  src/Vector/../../include/Vector/vector_define.h \
-  src/Vector/../../include/Vector/../struct.h
-
-src/Vector/../../include/Vector/vector_define.h:
-
-src/Vector/../../include/Vector/../struct.h:
--- objects/Vector/vector_quick_sort.d
+++
@@ -1,7 +0,0 @@
-objects/Vector/vector_quick_sort.o: src/Vector/vector_quick_sort.c \
-  src/Vector/../../include/Vector/vector_define.h \
-  src/Vector/../../include/Vector/../struct.h
-
-src/Vector/../../include/Vector/vector_define.h:
-
-src/Vector/../../include/Vector/../struct.h:
